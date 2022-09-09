8b400c6560f3f226325f0cef0e70594367625f64
Anzhy11 <harnzzy@gmail.com>
2022-09-09 09:57:26 +0000

    for statement with putchar

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lc, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		if (lc != e && lc != q)
+			putchar(lc);
+	}
+	putchar('\n');
+
+	return (0);
+}
