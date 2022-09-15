afc578e500557c590f182bec265a2224a5e93d10
Anzhy11 <harnzzy@gmail.com>
2022-09-15 09:53:00 +0000

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - Check main
+ * Description: function that prints 10 times the numbers,
+ * from 0 to 14, followed by a new line.
+ */
+void more_numbers(void)
+{
+	int i, j = 0;
+
+	while (j <= 9)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	j++;
+	}
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * void print_line - function that draws a straight line in the terminal.
+ * @n: An input integer
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		for (; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
