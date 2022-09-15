76a1051d11174816e90c1d6557becb97c1d0779b
Anzhy11 <harnzzy@gmail.com>
2022-09-15 08:36:46 +0000

    4-print_most_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Check main
+ * Description: function that prints the numbers
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
