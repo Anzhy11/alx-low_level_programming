4d8b536fc0c86b10ffc2470200613572fc6dbd17
Anzhy11 <harnzzy@gmail.com>
2022-09-30 09:09:49 +0000

    argc and argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+
+	return (1);
+}
