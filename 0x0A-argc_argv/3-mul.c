845cc4b36ff006c65b7922f80cc738760e7ae06a
Anzhy11 <harnzzy@gmail.com>
2022-09-30 11:34:10 +0000

    fixed 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,21 +6,13 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-
 int main(int argc, char *argv[])
 {
-	int mul;
-
-	if (argc == 3)
-	{
-		mul = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", mul);
-		return (1);
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
+		return (1);
 	}
-
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
