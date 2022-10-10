51b1c76ce6bd2d9b349051b9a6789d5379dc08bb
Anzhy11 <harnzzy@gmail.com>
2022-10-10 08:52:11 +0000

    Structures, typedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ *
+ * Return: struct dog.
+ * if fails, returns NULL.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *my_dog;
+
+	my_dog = malloc(sizeof(dog_t));
+	if (my_dog == NULL)
+		return (NULL);
+	my_dog->name = name;
+	my_dog->age = age;
+	my_dog->owner = owner;
+	return my_dog;
+}
